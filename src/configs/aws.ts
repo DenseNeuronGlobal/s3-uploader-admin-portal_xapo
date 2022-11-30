6206f957d78d0e13296354b53c80770d5435b99a
DenseNeuron <117843026+DenseNeuron@users.noreply.github.com>
2022-11-30 23:35:53 +0000

    Update aws.ts

--- src/configs/aws.ts
+++ src/configs/aws.ts
@@ -1,14 +1,14 @@
 export const COGNITO = {
-  REGION: process.env.REACT_APP_AWS_REGION || 'eu-west-2',
-  USER_POOL_ID: process.env.REACT_APP_AWS_USER_POOL_ID || 'eu-west-2_WF3U6P6RL',
-  IDENTITY_POOL_ID: process.env.REACT_APP_AWS_IDENTITY_POOL_ID || 'eu-west-2:99b19084-0ff8-4230-b3cf-f78424afed50',
-  APP_CLIENT_ID: process.env.REACT_APP_AWS_APP_CLIENT_ID || '35ltboj80ues7ueumsvmhcm95c',
-  S3_BUCKET: process.env.REACT_APP_AWS_S3_BUCKET || 'finaltest71325-dev',
+  REGION: process.env.REACT_APP_AWS_REGION || '',
+  USER_POOL_ID: process.env.REACT_APP_AWS_USER_POOL_ID || '',
+  IDENTITY_POOL_ID: process.env.REACT_APP_AWS_IDENTITY_POOL_ID || '',
+  APP_CLIENT_ID: process.env.REACT_APP_AWS_APP_CLIENT_ID || '',
+  S3_BUCKET: process.env.REACT_APP_AWS_S3_BUCKET || '',
   CUSTOM_LOGIC: process.env.REACT_APP_AWS_CUSTOM_LOGIC ? JSON.parse(process.env.REACT_APP_AWS_CUSTOM_LOGIC) : [
         {
-            "name": "AdminQueries",
-            "endpoint": "https://ax3ml60zwe.execute-api.eu-west-2.amazonaws.com/dev",
-            "region": "eu-west-2"
+            "name": "",
+            "endpoint": "",
+            "region": ""
         }
     ],
 };
